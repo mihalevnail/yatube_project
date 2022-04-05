e3791c6caed3fe2e3b0eae198f08fdd73b22a53d
Наиль Михалев <mihalev_nail@mail.ru>
2022-04-05 15:49:15 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse('Главная страница')
+
+def group_posts(request, pk):
+    return HttpResponse(f'Группа вот с таким названием {pk}')
